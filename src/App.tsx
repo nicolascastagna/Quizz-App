110c9864ff2585c1961e19aa53071a34556d9233
nicolascastagna <100592012+nicolascastagna@users.noreply.github.com>
2022-11-11 10:16:31 +0100

    mis en place de la structure fonctions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-function App() {
+const App = () => {
+
+  const startQuizz = async () => {
+    
+  }
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
-      Quizz
+      <h1>Quizz App</h1>
+      <button className="start" onClick={startQuizz}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions</p>
     </div>
   );
 }
