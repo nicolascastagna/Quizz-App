28d3fa86be031880b3fbbc05b8b1f5bfecac819f
nicolascastagna <100592012+nicolascastagna@users.noreply.github.com>
2022-11-12 14:52:48 +0100

    ajout logique de questions aléatoires et mise en place des réponses API

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
 import { useState } from "react";
 import QuestionCard from "./components/QuestionCard";
-import {Difficulty, fetchQuizzQuestions } from "./utils/API"
+import { Difficulty, fetchQuizzQuestions, QuestionsState } from './utils/API';
 
-const App = () => {
+type AnswerObject = {
+  question: string;
+  answer: string; 
+  correct: boolean;
+  correct_answer: string;
+  incorrect: boolean;
+  incorrect_answer: string;
+}
+
+const App: React.FC = () => {
 
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionsState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
   const [total, setTotal] = useState(10);
 
-  console.log(fetchQuizzQuestions(total, Difficulty.EASY));
-  
+  console.log(questions);
 
   const startQuizz = async () => {
-    
-  }
+  
+      setLoading(true);
+      setGameOver(false);
+      const newQuestions:any = await fetchQuizzQuestions(total, Difficulty.EASY);
+     
+      setQuestions(newQuestions);
+      setScore(0);
+      setUserAnswers([]);
+      setNumber(0);
+      setLoading(false);
+    }
+  
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
 
   }
@@ -29,9 +47,11 @@ const App = () => {
   return (
     <main className="main">
       <h1>Quizz App</h1>
+      {gameOver || userAnswers.length === total ? (
       <button className="start" onClick={startQuizz}>
         Start
       </button>
+      ) : null}
       <p className="score">Score:</p>
       <p>Loading Questions...</p>
       {/* <QuestionCard 
--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,13 +1,16 @@
 import axios from "axios";
+import { shuffleArray } from "./utils";
 
 export type Question = {
     category: string,
     correct_answer: string,
     difficulty: string,
-    incorrect_answer: string[],
+    incorrect_answers: string[],
     question: string,
     type: string,
 }
+
+export type QuestionsState = Question & { answers: string[] };
 
 export enum Difficulty {
     EASY = "easy",
@@ -17,12 +20,18 @@ export enum Difficulty {
 
 export const fetchQuizzQuestions = async (amount: number, difficulty: Difficulty) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
-    const data = await (await axios.get(endpoint, {
+    await (await axios.get(endpoint, {
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
         },
       }).then((res) => {
         console.log(res.data.results);
+        return res.data.results.map((question: Question) => (
+            {
+                ...question,
+                answers: shuffleArray([ ...question.incorrect_answers, question.correct_answer]),
+            }
+        ))
     }))
 }
